26f8572bbc19a9edbb98ddc1ebfb45048d5d86f1
june29 <june29.jp@gmail.com>
2017-02-09 00:18:07 +0900

    :sparkles: Act only on entry page

--- static/js/hugo29.js
+++ static/js/hugo29.js
@@ -1,19 +1,19 @@
 $(function() {
-  var already = $("#other-entries li a").map(function(_i, a) {
-    return $(a).attr("href").replace(/\/$/, "");
-  });
-
-  $.getJSON("/entries.json", function(entries) {
-    var sampled = _.reject(_.sample(entries, 12), function(entry) {
-      return _.contains(already, entry.link);
+  if ($("#other-entries").length > 0) {
+    var already = $("#other-entries li a").map(function(_i, a) {
+      return $(a).attr("href").replace(/\/$/, "");
     });
 
-    $.each(sampled, function(_i, entry) {
-      console.log(entry.link);
+    $.getJSON("/entries.json", function(entries) {
+      var sampled = _.reject(_.sample(entries, 12), function(entry) {
+        return _.contains(already, entry.link);
+      });
 
-      $("<li/>").append(
-        $("<a/>").attr("href", entry.link).text(entry.date + " : " + entry.title)
-      ).appendTo($("#other-entries ul"));
+      $.each(sampled, function(_i, entry) {
+        $("<li/>").append(
+          $("<a/>").attr("href", entry.link).text(entry.date + " : " + entry.title)
+        ).appendTo($("#other-entries ul"));
+      });
     });
-  });
+  }
 });
